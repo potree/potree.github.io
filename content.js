56abb9dc68d5488e87658c3f96a5b00090f48844
m-schuetz <markus_schuetz@gmx.at>
2017-07-23 17:02:18 +0200

    try hosting resources

--- content.js
+++ content.js
@@ -1,138 +1,138 @@
 let examples = [
 	{
-		link: "http://potree.org/showcase/retz.html",
+		link: "http://potree.org/demo/potree_1.5/examples/viewer.html",
+		thumb: './thumbnails/examples/viewer.png',
+		title: "Basic Viewer"
+	},{
+		link: "http://potree.org/demo/potree_1.5/examples/gradient_colors.html",
 		thumb: './thumbnails/examples/gradient_colors.png',
 		title: "Gradients"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/annotations.html",
 		thumb: './thumbnails/examples/annotations.png',
 		title: "Annotations"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/clipping_volume.html",
 		thumb: './thumbnails/examples/clipping_volumes.png',
 		title: "Clip Boxes"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/custom_sidebar_section.html",
 		thumb: './thumbnails/examples/custom_sidebar_section.png',
 		title: "Custom Sidebar Section"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/elevation_profile.html",
 		thumb: './thumbnails/examples/elevation_profile.png',
 		title: "Elevation Profile"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/embedded_iframe.html",
 		thumb: './thumbnails/examples/embedded_iframe.png',
 		title: "Embedded Iframe"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/entwine_greyhound.html",
 		thumb: './thumbnails/examples/entwine_greyhound.png',
 		title: "Entwine / Greyhound"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/lines.html",
 		thumb: './thumbnails/examples/lines.png',
 		title: "Lines"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/lion.html",
 		thumb: './thumbnails/examples/lion.png',
 		title: "Lion"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/lion_las.html",
 		thumb: './thumbnails/examples/lion_las.png',
 		title: "Lion - LAS"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/lion_laz.html",
 		thumb: './thumbnails/examples/lion_laz.png',
 		title: "Lion - LAZ"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/measurements.html",
 		thumb: './thumbnails/examples/measurements.png',
 		title: "Measurements"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/meshes.html",
 		thumb: './thumbnails/examples/meshes.png',
 		title: "Meshes"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/multiple_pointclouds.html",
 		thumb: './thumbnails/examples/multiple_point_clouds.png',
 		title: "Multiple Point Clouds"
 	},{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/demo/potree_1.5/examples/shapefiles.html",
 		thumb: './thumbnails/examples/shapefiles.png',
 		title: "Shapefiles"
-	},{
-		link: "http://potree.org/showcase/pompei.html",
-		thumb: './thumbnails/examples/viewer.png',
-		title: "Basic Viewer"
-	}
+	},
 ];
 
 let showcase = [
 	{
-		link: "http://potree.org/showcase/pompei.html",
+		link: "./showcase/ca13.html",
 		thumb: './thumbnails/showcase/CA13.jpg',
 		title: "CA13"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/chiller.html",
 		thumb: './thumbnails/showcase/chiller.jpg',
 		title: "Chiller"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/tern_auscover_chowilla.html",
 		thumb: './thumbnails/showcase/chowilla.jpg',
 		title: "Chowilla"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/cooler_tower.html",
 		thumb: './thumbnails/showcase/cooler_tower.jpg',
 		title: "Cooler"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/dechen_cave.html",
 		thumb: './thumbnails/showcase/dechen_cave.jpg',
 		title: "Dechen Cave"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/doverMillRuins.html",
 		thumb: './thumbnails/showcase/DoverMillRuins.jpg',
 		title: "Ruins"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/eclepens.html",
 		thumb: './thumbnails/showcase/eclepens.jpg',
 		title: "Eclepens"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/grab_15.html",
 		thumb: './thumbnails/showcase/grab_15.jpg',
 		title: "Grave"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/heidentor.html",
 		thumb: './thumbnails/showcase/heidentor.jpg',
 		title: "Heidentor"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/lake_tahoe.html",
 		thumb: './thumbnails/showcase/lake_tahoe.jpg',
 		title: "Lake Tahoe"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/land_building.html",
 		thumb: './thumbnails/showcase/land_building.jpg',
 		title: "Building"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/LDHI_module.html",
 		thumb: './thumbnails/showcase/LDHI_module.jpg',
 		title: "LDHI"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/lifeboat.html",
 		thumb: './thumbnails/showcase/lifeboat.jpg',
 		title: "Lifeboat"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/lion_head_simone_garagnani.html",
 		thumb: './thumbnails/showcase/lion_head.jpg',
 		title: "Lion Head"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/matterhorn.html",
 		thumb: './thumbnails/showcase/matterhorn.jpg',
 		title: "Matterhorn"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/overpass.html",
 		thumb: './thumbnails/showcase/overpass.jpg',
 		title: "Overpass"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/pielach.html",
 		thumb: './thumbnails/showcase/pielach.jpg',
 		title: "Pielach"
 	}, {
@@ -140,51 +140,35 @@ let showcase = [
 		thumb: './thumbnails/showcase/pompei.jpg',
 		title: "Pompei"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/retz.html",
 		thumb: './thumbnails/showcase/retz.jpg',
 		title: "Retz"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/santorini.html",
 		thumb: './thumbnails/showcase/santorini.jpg',
 		title: "Santorini"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/skatepark.html",
 		thumb: './thumbnails/showcase/skatepark.jpg',
 		title: "Skatepark"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
-		thumb: './thumbnails/showcase/solothurn.jpg',
-		title: "Solothurn"
-	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/subsea_equipment.html",
 		thumb: './thumbnails/showcase/subsea_equipment.jpg',
 		title: "Subsea Eq."
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/subsea_manifold.html",
 		thumb: './thumbnails/showcase/subseamanifold.jpg',
 		title: "Subsea Man."
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
-		thumb: './thumbnails/showcase/Sunset_Crater.jpg',
-		title: "Sunset Crater"
-	}, {
-		link: "http://potree.org/showcase/pompei.html",
-		thumb: './thumbnails/showcase/vessel.jpg',
-		title: "Vessel"
-	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/sorvilier.html",
 		thumb: './thumbnails/showcase/vol_total.jpg',
 		title: "Sorvilier"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
-		thumb: './thumbnails/showcase/watertank.jpg',
-		title: "Watertank"
-	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/westend_palais.html",
 		thumb: './thumbnails/showcase/westend_palais.jpg',
 		title: "Westend Palais"
 	}, {
-		link: "http://potree.org/showcase/pompei.html",
+		link: "http://potree.org/showcase/whitby.html",
 		thumb: './thumbnails/showcase/whitby.jpg',
 		title: "Whitby"
 	}
